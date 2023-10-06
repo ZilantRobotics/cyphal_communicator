1a8cf79bc0852c1927af5c5bb6763aabe7532a86
PonomarevDA <ponomarevda96@gmail.com>
2023-10-06 23:26:27 +0300

    update create_slcan.sh and call this script with `./scripts/docker.sh r` or `./scripts/docker.sh i`

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-sudo apt install -y ros-$ROS_DISTRO-mavros-msgs
+sudo apt install -y ros-$ROS_DISTRO-mavros-msgs can-utils net-tools iproute2
 
 python3 -m pip install --upgrade pip
 python3 -m pip install -r $SCRIPT_DIR/requirements.txt
