86da4c3d4d7a1a2eef3cca4c3be86c336f067d93
PonomarevDA <ponomarevda96@gmail.com>
2023-11-09 00:37:35 +0300

    round battery current more correctly

--- src/autopilot_interface/cyphal_hitl.cpp
+++ src/autopilot_interface/cyphal_hitl.cpp
@@ -103,8 +103,8 @@ void CyphalHitlInterface::publish_esc_feedback(uint8_t esc_idx, float voltage, f
     }
 
     ZubaxCompactFeedback feedback;
-    feedback.dc_voltage = voltage * 5.0;
-    feedback.dc_current = current * 5.0;
+    feedback.dc_voltage = std::round(voltage * 5.0f);
+    feedback.dc_current = std::round(current * 5.0f);
     feedback.velocity = rpm * ZubaxCompactFeedbackPublisher::RPM_TO_RAD_PER_SEC;
 
     esc_feedback[esc_idx]->publish(feedback);
